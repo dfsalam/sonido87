80354bb361136846df32d5b0402da0fc2dae4258
dfsalam <dfsalam@gmail.com>
2023-07-25 22:08:19 -0500

    Add an error page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './routes/Home';
 import About from './routes/About';
 import Login from './routes/Login';
 import Profile from './routes/Profile';
+import NotMatch from './routes/NotMatch';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="about" element={<About />} />
       <Route path="login" element={<Login />} />
       <Route path="profile" element={<Profile />} />
+      <Route path="*" element={<NotMatch />} />
     </Routes>
   );
 };
