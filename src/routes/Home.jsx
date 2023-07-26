b0af8d300ef2afec7926939ce650cb3734035803
dfsalam <dfsalam@gmail.com>
2023-07-25 22:05:32 -0500

    Update routes on App component.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,8 @@
-import Header from '@/components/Header';
-import TodosLogic from '@/components/TodosLogic';
-
 const Home = () => {
   return (
     <div className="wrapper">
       <div className="todos">
-        <Header />
-        <TodosLogic />
+        Header
       </div>
     </div>
   );
