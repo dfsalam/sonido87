efc6d6ef61b3c50872a65d73695dcf3b7468ef9e
dfsalam <dfsalam@gmail.com>
2023-07-25 22:34:34 -0500

    Add outlet component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,10 @@
 import Navbar from './Navbar';
+import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
     <div className="wrapper">
       <Navbar />
+      <Outlet />
     </div>
   );
 };
