e0905c4bc5b2b207a12aa0e4bf9b697313c6f055
dfsalam <dfsalam@gmail.com>
2023-07-26 22:51:00 -0500

    Create a redux state slice.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import { store } from './app/store'
-import { Provider } from 'react-redux'
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 
