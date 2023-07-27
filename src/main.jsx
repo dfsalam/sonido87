5c22c556791f6f4fca665ed537ba2ab486ddbb65
dfsalam <dfsalam@gmail.com>
2023-07-26 22:46:10 -0500

    Provide the Redux Store to React.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Provider store={store}>
+      <App />    
+    </Provider>      
     </BrowserRouter>
   </React.StrictMode>,
 );
