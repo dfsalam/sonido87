46ae5199e66e14eef3c45750a7af494db9f8fb7c
dfsalam <dfsalam@gmail.com>
2023-07-26 22:58:23 -0500

    Fix linter errors.

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-      <App />    
-    </Provider>      
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
