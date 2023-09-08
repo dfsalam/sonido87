b1a3889e98411046c81b4469c10d17051582eb45
dfsalam <dfsalam@gmail.com>
2023-09-07 23:31:40 -0500

    Add sign-up form

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,2 +1,18 @@
-const Login = () => <div>Login page</div>;
-export default Login;
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+      <h3>Sign up</h3>
+      <form id="sign_up_form">
+        <input type="text" id="sign_up_email" placeholder="Email" />
+        <input type="password" id="sign_up_password" placeholder="Password" />
+        <input type="password" id="sign_up_password_confirm" placeholder="Confirm password" />
+        <button type="submit" id="sign_up_button">Submit</button>
+      </form>
+
+    </>
+  )
+}
+
+export default Login
