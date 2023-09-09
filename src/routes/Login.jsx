d9d80fb3c7247c9fcc9eba87b26ecd091aed448d
dfsalam <dfsalam@gmail.com>
2023-09-08 22:43:53 -0500

    Refactor sign-up form

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
 
 const Login = () => {
-  return (
-    <>
+  return (    
+    <form id="sign_up_form">
       <h3>Sign up</h3>
-      <form id="sign_up_form">
-        <input type="text" id="sign_up_email" placeholder="Email" />
-        <input type="password" id="sign_up_password" placeholder="Password" />
-        <input type="password" id="sign_up_password_confirm" placeholder="Confirm password" />
-        <button type="submit" id="sign_up_button">Submit</button>
-      </form>
-
-    </>
+      <input type="text" id="sign_up_email" placeholder="Email" />
+      <input type="password" id="sign_up_password" placeholder="Password" />
+      <input type="password" id="sign_up_password_confirm" placeholder="Confirm password" />
+      <button type="submit" id="sign_up_button">Submit</button>
+    </form>    
   )
 }
 
