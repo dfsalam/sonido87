45765da58e36bf5aff50654931f615f71467f3e1
dfsalam <dfsalam@gmail.com>
2023-09-12 18:57:57 -0500

    Add logout button

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -5,19 +5,21 @@ const Login = () => {
   return (
     <>
       <form id="sign_up_form">
-      <h3>Sign up</h3>
-      <input type="text" id="sign_up_email" placeholder="Email" />
-      <input type="password" id="sign_up_password" placeholder="Password" />
-      <input type="password" id="sign_up_password_confirm" placeholder="Confirm password" />
-      <button type="submit" id="sign_up_button">Submit</button>
+        <h3>Sign up</h3>
+        <input type="text" id="sign_up_email" placeholder="Email" />
+        <input type="password" id="sign_up_password" placeholder="Password" />
+        <input type="password" id="sign_up_password_confirm" placeholder="Confirm password" />
+        <button type="submit" id="sign_up_button">Submit</button>
       </form>
 
       <form id="login_form">
-      <h3>Login</h3>
-      <input type="text" id="login_email" placeholder="Email" />
-      <input type="password" id="login_password" placeholder="Password" />      
-      <button type="submit" id="login_button">Submit</button>
-      </form>  
+        <h3>Login</h3>
+        <input type="text" id="login_email" placeholder="Email" />
+        <input type="password" id="login_password" placeholder="Password" />      
+        <button type="submit" id="login_button">Submit</button>
+      </form>
+
+      <button type="button" id="logout_button">Logout</button>
     </>    
        
   )
