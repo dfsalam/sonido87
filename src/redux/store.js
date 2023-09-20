cd17bd9a6192bc5a24f9688ac6f75cb9b1fd6998
dfsalam <dfsalam@gmail.com>
2023-09-19 23:56:40 -0500

    Add userSlice to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counterSlice';
+import userSlice from './userSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    user: userReducer,
   },
 });
 
